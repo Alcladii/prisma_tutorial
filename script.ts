3edda9df4d5bf80fbb4d54aa4d9558a323bdda1a
Alcladii <122701312+Alcladii@users.noreply.github.com>
2025-08-14 00:09:37 +0100

    where clause in findMany

--- script.ts
+++ script.ts
@@ -2,11 +2,23 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
+  //await prisma.user.deleteMany()
+  // const users = await prisma.user.createMany({
+  //   data: [
+  //     { name: "Sarra", age: 29, email: "sarra@example1.com" },
+  //     { name: "John", age: 20, email: "john@example1.com" },
+  //   ],
+  // })
   //await prisma.user.deleteMany(); 
   const users = await prisma.user.findMany({
     where: {
       name: "Sarra"
     },
+    orderBy: {
+      age: "desc",
+    },
+    take: 2,
+    //skip: 1,
   });
   console.log(users);
 }
